028a072fed689222ee6d6d00ca1f6012c0043616
ofirbo <ofirbo@gmail.com>
2016-01-25 14:21:22 +0200

    Scatter Plot Control for C# WinForm

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scatterPlot1 = new Scatter.ScatterPlot();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar2.Location = new System.Drawing.Point(93, 330);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(179, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "F";
+            // 
+            // scatterPlot1
+            // 
+            this.scatterPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scatterPlot1.Azimuth = 0D;
+            this.scatterPlot1.CameraPos = new double[] {
+        0D,
+        0D,
+        -5D};
+            this.scatterPlot1.Distance = 5D;
+            this.scatterPlot1.Elevation = 0D;
+            this.scatterPlot1.F = 1000D;
+            this.scatterPlot1.Location = new System.Drawing.Point(12, 12);
+            this.scatterPlot1.Name = "scatterPlot1";
+            this.scatterPlot1.Size = new System.Drawing.Size(475, 312);
+            this.scatterPlot1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 368);
+            this.Controls.Add(this.scatterPlot1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private Scatter.ScatterPlot scatterPlot1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scatterPlot1.Clear();
+            Random rand = new Random();
+            double R = 1;
+            List<double[]> Points = new List<double[]>();
+
+            for (int j = 0; j < 5; j++)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    double theta = Math.PI * rand.NextDouble();
+                    double phi = 2 * Math.PI * rand.NextDouble();
+                    double x = R * Math.Sin(theta) * Math.Cos(phi);
+                    double y = R * Math.Sin(theta) * Math.Sin(phi);
+                    double z = R * Math.Cos(theta);
+                    Points.Add(new double[] { x, y, z });
+                }
+                scatterPlot1.AddPoints(Points);
+
+                Points.Clear();
+            }
+
+            for (int i = 0; i < 200; i++)
+            {
+                //double theta = Math.PI * rand.NextDouble();
+                //double phi = 2 * Math.PI * rand.NextDouble();
+                double theta = 10D / 180 * Math.PI * Math.Sin(10 * 2 * Math.PI * i / 200);
+                double phi = 2 * Math.PI * i / 200;
+                double x = R * Math.Cos(theta) * Math.Cos(phi);
+                double y = R * Math.Cos(theta) * Math.Sin(phi);
+                double z = R * Math.Sin(theta);
+                Points.Add(new double[] { x, y, z });
+                
+            }
+            scatterPlot1.AddPoints(Points);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            scatterPlot1.F = trackBar2.Value / 100D * 1000;
+        }
+    }
+}
